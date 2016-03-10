6d22373017c19177d15aa8f5a0e54e84168292c5
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-03-09 21:43:28 -0800

    Redid project org; first exercise from operators chapter

---
+++ src/Ex1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by curtismitchell on 3/9/16.
+ */
+import java.util.*;
+
+public class Ex1 {
+    static void print(Object line) {
+        System.out.println(line);
+    }
+
+    public static void main(String[] args) {
+        print("Hello, world!");
+        System.out.println("Hello, world!");
+    }
+}
