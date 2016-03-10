7c0b33ccd286158b9be50f9bcf9ec8f7f3f6fb0a
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-03-09 21:49:04 -0800

    Operators ex2

---
+++ src/Ex2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by curtismitchell on 3/9/16.
+ */
+public class Ex2 {
+    public static void main(String[] args) {
+        float f1 = 0;
+        float f2 = 1;
+        System.out.println("f1: " + f1);
+        System.out.println("f2: " + f2);
+        f2 = f1;
+        System.out.println("f1: " + f1);
+        System.out.println("f2: " + f2);
+    }
+}
