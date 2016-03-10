07de0f68c1e7661f3522bbdee07db5e78e7deb6b
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-03-09 21:57:08 -0800

    Corrected Ex2

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,14 +1,24 @@
 /**
  * Created by curtismitchell on 3/9/16.
  */
+class Tank {
+    float level;
+}
+
 public class Ex2 {
     public static void main(String[] args) {
-        float f1 = 0;
-        float f2 = 1;
-        System.out.println("f1: " + f1);
-        System.out.println("f2: " + f2);
-        f2 = f1;
-        System.out.println("f1: " + f1);
-        System.out.println("f2: " + f2);
+        Tank t1 = new Tank();
+        Tank t2 = new Tank();
+        t1.level = 11;
+        t2.level = 33;
+
+        System.out.println("t1.level: " + t1.level);
+        System.out.println("t2.level: " + t2.level);
+        t1 = t2;
+        System.out.println("t1.level: " + t1.level);
+        System.out.println("t2.level: " + t2.level);
+        t1.level = 50;
+        System.out.println("t1.level: " + t1.level);
+        System.out.println("t2.level: " + t2.level);
     }
 }
