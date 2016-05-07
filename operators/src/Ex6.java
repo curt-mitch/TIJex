426329b01409d8075c29384b131256f3667bd8e0
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-05-07 13:39:14 -0400

    Operators ex 6

--- operators/src/Ex6.java
+++ operators/src/Ex6.java
@@ -1,5 +1,24 @@
 /**
  * Created by curtismitchell on 5/7/16.
  */
-public class Ex6 {
+
+class DogTest {
+    String name;
+    String says;
 }
+
+public class Ex6 {
+
+    public static void main(String[] args) {
+        DogTest spot = new DogTest();
+        spot.name = "spot";
+        spot.says = "Ruff!";
+
+        DogTest spotCopy = spot;
+
+        System.out.println("does spotCopy equal spot?: " + spot.equals(spotCopy));
+        System.out.println("does spotCopy.name equal spot.name?: " + spot.name == spotCopy.name);
+        System.out.println("does spotCopy.says equal spot.says?: " + spot.says == spotCopy.says);
+    }
+}
+
