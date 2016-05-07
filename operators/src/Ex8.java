5a737eb6585f56df0a15d51e7c0e86aab08b15b7
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-05-07 14:01:56 -0400

    Operators ex 8

---
+++ operators/src/Ex8.java
@@ -0,0 +1,12 @@
+/**
+ * Created by curtismitchell on 5/7/16.
+ */
+public class Ex8 {
+    public static void main(String[] args) {
+        long n1 = 0x2f;
+        long n2 = 0177;
+
+        System.out.println(Long.toBinaryString(n1));
+        System.out.println(Long.toBinaryString(n2));
+    }
+}
