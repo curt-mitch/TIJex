e511ced0680d5b5efb58d9ed23e75f4d0ab7e794
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-05-07 14:24:39 -0400

    Operators ex 9

---
+++ operators/src/Ex9.java
@@ -0,0 +1,15 @@
+/**
+ * Created by curtismitchell on 5/7/16.
+ */
+public class Ex9 {
+    public static void main(String[] arg) {
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+        System.out.println("maxDouble: " + maxDouble);
+        System.out.println("minDouble: " + minDouble);
+        System.out.println("maxFloat: " + maxFloat);
+        System.out.println("minFloat: " + minFloat);
+    }
+}
